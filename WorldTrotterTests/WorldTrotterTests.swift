2944e937e5492b97d0b132cbedd6c0baf8fdbacb
RedBeard41 <42767289+RedBeard41@users.noreply.github.com>
2026-01-31 14:24:24 -0500

    Initial Commit

---
+++ WorldTrotterTests/WorldTrotterTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTrotterTests.swift
+//  WorldTrotterTests
+//
+//  Created by Gabes mac on 1/31/26.
+//
+
+import Testing
+@testable import WorldTrotter
+
+struct WorldTrotterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
