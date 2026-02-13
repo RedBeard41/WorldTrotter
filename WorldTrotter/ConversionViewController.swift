ef051ae90f09c14e4a6b51bf4620fdbd85e7bebb
RedBeard41 <42767289+RedBeard41@users.noreply.github.com>
2026-02-13 11:28:38 -0500

    Add all labels prgromatically

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -9,6 +9,56 @@ import UIKit
 
 class ConversionViewController: UIViewController {
 
+    
+    override func loadView() {
+        super.loadView()
+        
+        let view = UIView()
+        
+        view.backgroundColor = UIColor(red: 245/255, green: 244/255, blue: 241/255, alpha: 0.85)
+        self.view = view
+        
+        let customColor = UIColor(red: 225/255, green: 88/255, blue: 41/255, alpha: 1)
+        let largeFontSize = CGFloat(70)
+        let smallFontSize = CGFloat(30)
+        
+        
+        let tempFahrenheitLabel = UILabel()
+        tempFahrenheitLabel.text = "212"
+        tempFahrenheitLabel.textColor = customColor
+        tempFahrenheitLabel.font = UIFont.systemFont(ofSize: largeFontSize)
+        tempFahrenheitLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        let fLabel = UILabel()
+        fLabel.text = "degrees Fahrenheit"
+        fLabel.textColor = customColor
+        fLabel.font = UIFont.systemFont(ofSize: smallFontSize)
+        fLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let compareLabel = UILabel()
+        compareLabel.text = "is equal to"
+        compareLabel.textColor = .black
+        compareLabel.font = UIFont.systemFont(ofSize: smallFontSize)
+        compareLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let tempCelsiusLabel = UILabel()
+        tempCelsiusLabel.text = "100"
+        tempCelsiusLabel.textColor = customColor
+        tempCelsiusLabel.font = UIFont.systemFont(ofSize: largeFontSize)
+        tempCelsiusLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let cLabel = UILabel()
+        cLabel.text = "degrees Celsius"
+        cLabel.textColor = customColor
+        cLabel.font = UIFont.systemFont(ofSize: smallFontSize)
+        cLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        
+       
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
