5ff39e8b14aef357a15ae1307aa75b52eb7bfcea
RedBeard41 <42767289+RedBeard41@users.noreply.github.com>
2026-02-12 23:38:56 -0500

    refactored mapViewtype to take run from UISwitch and Segmented Control

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -12,12 +12,14 @@ import MapKit
 class MapViewController: UIViewController {
     
     var mapView: MKMapView!
+    let interestSwitch = UISwitch()
+    let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
     
     override func loadView() {
         mapView = MKMapView()
         view = mapView
         
-        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+//        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         segmentedControl.backgroundColor = UIColor.systemBackground
         segmentedControl.selectedSegmentIndex = 0
         
@@ -50,8 +52,17 @@ class MapViewController: UIViewController {
         pointInterest.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
         pointInterest.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 20).isActive = true
         
-
-    }
+//        let interestSwitch = UISwitch()
+        interestSwitch.isOn = true
+        
+        interestSwitch.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(interestSwitch)
+        
+        interestSwitch.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 20).isActive = true
+        interestSwitch.leadingAnchor.constraint(equalTo: pointInterest.trailingAnchor, constant: 16).isActive = true
+        interestSwitch.addTarget(self, action: #selector(mapTypeChanged(_:)), for: .valueChanged)
+        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -75,18 +86,36 @@ class MapViewController: UIViewController {
 //        print("MapViewController did disappear")
     }
     
-    @objc func mapTypeChanged(_ segmentedControl: UISegmentedControl) {
+    @objc func mapTypeChanged(_ sender: Any) {
+        
+        let filter = interestSwitch.isOn ? MKPointOfInterestFilter.includingAll : MKPointOfInterestFilter.excludingAll
+        
         switch segmentedControl.selectedSegmentIndex {
         case 0:
             mapView.mapType = .standard
+            
+            let configuration = MKStandardMapConfiguration()
+            configuration.pointOfInterestFilter = filter
+            mapView.preferredConfiguration = configuration
+            
         case 1:
             mapView.mapType = .hybrid
+            
+            let configuration = MKHybridMapConfiguration()
+            configuration.pointOfInterestFilter = filter
+            mapView.preferredConfiguration = configuration
+            
         case 2:
             mapView.mapType = .satellite
         default:
             break
         }
+        
+        
+        
     }
+    
+    
     
 }
 
